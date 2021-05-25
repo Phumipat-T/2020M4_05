417cbd6624c783fa7b3ff70d1e43937b57523d48
Phumipat Thirawat <phumipat_thirawat@elearning.cmu.ac.th>
2021-05-25 22:37:11 +0700

    Commit3

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//ประกาศตัวแปร
 let stored = "0";
 let current = "0";
 let operation = 0;
 let MaxLength = 10;
 
+//ปุ่มตัวเลข
 function addDigit(dig_obj) {
     dig = dig_obj.value;
     if (current.length > MaxLength) {
@@ -13,21 +15,27 @@ function addDigit(dig_obj) {
     current = current + dig;
     }
     document.getElementById("display").innerHTML=current;
-   }
-   function addDot() {
+}
+
+//ปุ่มจุดทศนิยม
+function addDot() {
     if ( current.length == 0) {
     current = "0.";
     } else if ( current.indexOf(".") == -1) {
     current = current + ".";
     }
     document.getElementById("display").innerHTML=current;
-   }
-   function clearDisp() {
+}
+
+//ปุ่ม clear
+function clearDisp() {
     stored = "0";
     current = "0";
     document.getElementById("display").innerHTML=current;
-   }
-   function operate(op_obj) {
+}
+
+//ปุ่ม + - * /
+function operate(op_obj) {
     op = op_obj.value;
     if (op.indexOf("+") > -1) { operation = 1; }
     if (op.indexOf("-") > -1) { operation = 2; }
@@ -35,8 +43,10 @@ function addDigit(dig_obj) {
     if (op.indexOf("/") > -1) { operation = 4; }
     stored = current;
     current = "";
-   }
-   function calculate() {
+}
+
+//ปุ่ม =
+function calculate() {
     tmp = current;
     if (operation == 1) { current = eval(stored) + eval(current); };
     if (operation == 2) { current = eval(stored) - eval(current); };
@@ -47,16 +57,67 @@ function addDigit(dig_obj) {
     }
     document.getElementById("display").innerHTML=current;
     stored = current;
-   } 
+}
+
+//อื่นๆ
+function toggle() {
+       current = -1 * current;
+       document.getElementById("display").innerHTML=current;
+} 
    
-   document.getElementById("clear").addEventListener("click",clearDisp);
-   document.getElementById("dot").addEventListener("click",addDot);
-   document.getElementById("eval").addEventListener("click",calculate);
-   let all_num = document.getElementsByClassName("num")
-   for ( let i=0 ; i<all_num.length ; i++) {
+function percent() {
+       current = current/100;
+       document.getElementById("display").innerHTML=current;
+}
+   
+function x2() {
+        current = current * current;
+        document.getElementById("display").innerHTML=current;
+}
+   
+function xx() {
+       current = Math.pow(2, current);
+       document.getElementById("display").innerHTML=current;
+}
+   
+function x10() {
+       current = Math.pow(10, current);
+       document.getElementById("display").innerHTML=current;
+}
+   
+function x1() {
+       current = 1/current;
+       document.getElementById("display").innerHTML=current;
+}
+   
+function xroot2() {
+        current = Math.sqrt(current);
+        document.getElementById("display").innerHTML=current;
+}
+   
+function baselog() {
+       current = Math.log(current);
+       document.getElementById("display").innerHTML=current;
+}
+
+//ผูกปุ่มต่างๆกับฟังก์ชัน
+document.getElementById("clear").addEventListener("click",clearDisp);
+document.getElementById("dot").addEventListener("click",addDot);
+document.getElementById("eval").addEventListener("click",calculate);
+document.getElementById("toggle").addEventListener("click",toggle);
+document.getElementById("percent").addEventListener("click",percent);
+document.getElementById("x2").addEventListener("click",x2);
+document.getElementById("2x").addEventListener("click",xx);
+document.getElementById("10x").addEventListener("click",x10);
+document.getElementById("1/x").addEventListener("click",x1);
+document.getElementById("2√x").addEventListener("click",xroot2);
+document.getElementById("lnx").addEventListener("click",baselog);
+let all_num = document.getElementsByClassName("num")
+for ( let i=0 ; i<all_num.length ; i++) {
     all_num[i].addEventListener("click",function() {addDigit(this);});
-   }
-   let all_ops = document.getElementsByClassName("ops")
-   for ( let i=0 ; i<all_ops.length ; i++) {
+}
+let all_ops = document.getElementsByClassName("ops")
+for ( let i=0 ; i<all_ops.length ; i++) {
     all_ops[i].addEventListener("click",function() {operate(this);});
-   }
+}
+
