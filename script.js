7461595ee0d8a15f338aab68dd3c49172ff6fc5a
Phumipat Thirawat <phumipat_thirawat@elearning.cmu.ac.th>
2021-05-28 05:37:08 +0700

    Commit5

--- script.js
+++ script.js
@@ -60,11 +60,10 @@ function calculate() {
     if (operation == 7) { current = Math.log(eval(current)) / Math.log(eval(stored)); };
     if (current.toString().length > MaxLength) {
     current = "Too long";
-}
-
+    }
     document.getElementById("display").innerHTML=current;
     stored = current;
-    }   
+}
 
 function toggle() {
        current = -1 * current;
@@ -75,7 +74,7 @@ function percent() {
        current = current/100;
        document.getElementById("display").innerHTML=current;
 }
-
+   
 function x2() {
         current = current * current;
         document.getElementById("display").innerHTML=current;
@@ -85,37 +84,76 @@ function xx() {
        current = Math.pow(2, current);
        document.getElementById("display").innerHTML=current;
 }
-
+   
 function x10() {
        current = Math.pow(10, current);
        document.getElementById("display").innerHTML=current;
 }
-
+   
 function x1() {
        current = 1/current;
        document.getElementById("display").innerHTML=current;
 }
-
+   
 function xroot2() {
         current = Math.sqrt(current);
         document.getElementById("display").innerHTML=current;
 }
-
+   
 function baselog() {
        current = Math.log(current);
        document.getElementById("display").innerHTML=current;
 }
-
+   
 function e() {
     current = current * Math.E;
     document.getElementById("display").innerHTML=current;
 }
-
+    
 function pi() {
         current = current * Math.PI;
         document.getElementById("display").innerHTML=current;
 }
 
+function rand() {
+        current = Math.random();
+        document.getElementById("display").innerHTML=current;
+}
+    
+function factorial() {
+        const number = current;
+        // checking if number is negative
+        if (current < 0) {
+            current = "Error!"
+        }
+        // if number is 0
+        else if (number === 0) {
+            number = 1
+        }
+        // if number is positive
+        else {
+            let fact = 1;
+            for (i = 1; i <= number; i++) {
+                fact *= i;
+            }
+            current = fact;
+        }
+        document.getElementById("display").innerHTML=current;
+}
+    
+function sumx() {
+        if(current<0){
+            current = "Error!";
+        }else{
+            let sum = 0;
+            for(i = 0;i<=current;i++){
+                sum = sum + i;
+            }
+            current = sum;
+        }
+        document.getElementById("display").innerHTML=current;
+}
+   
 document.getElementById("clear").addEventListener("click",clearDisp);
 document.getElementById("dot").addEventListener("click",addDot);
 document.getElementById("eval").addEventListener("click",calculate);
@@ -129,6 +167,9 @@ document.getElementById("2√x").addEventListener("click",xroot2);
 document.getElementById("lnx").addEventListener("click",baselog);
 document.getElementById("e").addEventListener("click",e);
 document.getElementById("pi").addEventListener("click",pi);
+document.getElementById("fact").addEventListener("click",factorial);
+document.getElementById("sumx").addEventListener("click",sumx);
+document.getElementById("primex").addEventListener("click",primex);
    let all_num = document.getElementsByClassName("num")
    for ( let i=0 ; i<all_num.length ; i++) {
     all_num[i].addEventListener("click",function() {addDigit(this);});
